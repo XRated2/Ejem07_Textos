a4c0827f4c93322890496d5bff171abd986d3209
jsalor <jorgesalor24@gmail.com>
2016-04-28 09:03:43 +0200

    modificasion
    estaba mal, lo mio esta mejor jajaj xD pa k kieres saber eso jaja salu2

--- StringArrayUtilsTest.java
+++ StringArrayUtilsTest.java
@@ -1,19 +1,28 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class StringArrayUtilsTest {
 
+	@Parameters
+	public static ArrayList<String[]> misValores() {
+		ArrayList<String[]> datos = new ArrayList<String[]>();
+		
+		return datos;
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
-	
-	@Parameters
-	public static ArrayList<Complejo[]> misValores(){
+   
+	public void getMaxLengthTest(){
 		
 	}
 }
